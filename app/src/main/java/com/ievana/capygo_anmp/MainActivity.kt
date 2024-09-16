1e93f7e90e45a2ca68b68818ab932d3e7487d438
Jinninjilin <s160421008@student.ubaya.ac.id>
2024-09-16 16:36:37 +0700

    navigation

--- app/src/main/java/com/ievana/capygo_anmp/MainActivity.kt
+++ app/src/main/java/com/ievana/capygo_anmp/MainActivity.kt
@@ -2,10 +2,34 @@ package com.ievana.capygo_anmp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.ievana.capygo_anmp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var  binding:ActivityMainBinding
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //navcontroller diambil dari navhostfragment
+        navController =
+            (supportFragmentManager.findFragmentById(R.id.hostFragment)
+                    as NavHostFragment).navController
+
+
+        binding.bottomNav.setupWithNavController(navController)
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+
 }
