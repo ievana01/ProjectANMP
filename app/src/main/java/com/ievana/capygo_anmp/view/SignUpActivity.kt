f6bbf6079c8cba0769ce0c21656eaa94bf3a2ff7
ievana01 <118661162+ievana01@users.noreply.github.com>
2024-11-24 13:13:24 +0700

    xml login regis

---
+++ app/src/main/java/com/ievana/capygo_anmp/view/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.ievana.capygo_anmp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ievana.capygo_anmp.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
