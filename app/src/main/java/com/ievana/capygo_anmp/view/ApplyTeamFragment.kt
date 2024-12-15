a4172f8eddc4fbb123c31dc25f6830f61f88f22f
ievana01 <118661162+ievana01@users.noreply.github.com>
2024-12-15 15:35:06 +0700

    ui apply team

---
+++ app/src/main/java/com/ievana/capygo_anmp/view/ApplyTeamFragment.kt
@@ -0,0 +1,18 @@
+package com.ievana.capygo_anmp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ievana.capygo_anmp.R
+
+class ApplyTeamFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_apply_team, container, false)
+    }
+}
