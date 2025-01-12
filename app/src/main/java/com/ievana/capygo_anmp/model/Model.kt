9a74889d4926fd1ef2ebdf990e10f7c1e4fd8f2c
ievana01 <118661162+ievana01@users.noreply.github.com>
2025-01-12 10:45:45 +0700

    Revert "query"
    This reverts commit 5bd1a52bf51137a7c3e1d2cd7cf4c35a821417af.

--- app/src/main/java/com/ievana/capygo_anmp/model/Model.kt
+++ app/src/main/java/com/ievana/capygo_anmp/model/Model.kt
@@ -22,6 +22,7 @@ data class User(
     var uuid:Int =0
 }
 
+
 @Entity
 data class Proposal(
     @ColumnInfo(name="gameName")
@@ -36,92 +37,54 @@ data class Proposal(
     @PrimaryKey(autoGenerate = true)
     var id_prop:Int =0
 }
-@Entity
+
 data class Game(
-//    var id: String,
-    @ColumnInfo(name = "name")
+    var id: String,
     var name: String?,
-    @ColumnInfo(name = "description")
     var description: String?,
-    @ColumnInfo(name = "image")
     var image: String?,
     var teams: ArrayList<Team>,
     var achievements : List<Achievement>
-){
-    @PrimaryKey(autoGenerate = true)
-    var id:Int = 0
-}
+)
 
-@Entity
 data class Achievement(
-    @ColumnInfo(name = "year")
     var year: Int?,
-    @ColumnInfo(name = "winningTeam")
     var winningTeam: String?,//nama team
-    @ColumnInfo(name = "competitionTitle")
     var competitionTitle: String? //list competition
 )
 
-@Entity
 data class Team(
     //untuk team page dan who we are
-//    var id:String,
-    @ColumnInfo(name = "gameName")
+    var id:String,
     var gameName:String,
-    @ColumnInfo(name = "teamName")
     var teamName: String,
-    @ColumnInfo(name = "desc")
     var desc:String, //who we are
-    @ColumnInfo(name = "like")
     var like: Int, //who we are
-    @ColumnInfo(name = "imageTeam")
     var imageTeam: String?,
     var teamMember:ArrayList<Member>,
-){
-    @PrimaryKey(autoGenerate = true)
-    var idTeam:Int = 0
-}
+)
 
-@Entity
 data class Member(
     // untuk team page detail
-//    var id:String,
-    @ColumnInfo(name = "memberName")
+    var id:String,
     var memberName: String,
-    @ColumnInfo(name = "role")
     var role: String,
-    @ColumnInfo(name = "memberImage")
     var memberImage: String?
-){
-    @PrimaryKey(autoGenerate = true)
-    var idMember:Int = 0
-}
+)
 
-@Entity
 data class Schedule(
-//    var id: String,
-    @ColumnInfo(name = "date")
+    var id: String,
     var date: String?,
-    @ColumnInfo(name = "time")
     var time: String?,
-    @ColumnInfo(name = "ytLs")
     @SerializedName("youtubeLiveStream")
     var ytLS:String?,
-    @ColumnInfo(name = "gameName")
     var gameName: String?,
-    @ColumnInfo(name = "team")
     var team: String?,
-    @ColumnInfo(name = "location")
     var location:String?,
-    @ColumnInfo(name = "desc")
     @SerializedName("eventDescription")
     var desc:String?,
-    @ColumnInfo(name = "gameImage")
     var gameImage: String?
-){
-    @PrimaryKey(autoGenerate = true)
-    var idSchedule:Int = 0
-}
+)
 
 
 
